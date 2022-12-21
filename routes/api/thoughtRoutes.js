06c9f90813b294ad7bf57afe6f44871fd8caeed5
Hshuttle <huntershutle@gmail.com>
2022-12-20 19:00:28 -0500

    added new functions to the thoughtroutes page

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,8 +3,13 @@ const {
   getThoughts,
   getSingleThought,
   createThought,
+  deleteThought,
+  addReaction,
+  removeReaction
 } = require("../../controllers/thoughtController");
 router.route("/").get(getThoughts).post(createThought);
 router.route("/:thoughtId").get(getSingleThought);
+router.route("/").get(getThoughts).post(deleteThought);
+router.route("/:userId/thoughts/:thoughtId").put(addReaction).delete(removeReaction)
 
 module.exports = router;
